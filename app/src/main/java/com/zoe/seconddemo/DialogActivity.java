221d17a160b2e8dd5eece6dc2d9643347b979a05
ZoeWangW <13438165026@163.com>
2018-08-27 16:44:10 +0800

    Signed-off-by: ZoeWangW <13438165026@163.com>

---
+++ app/src/main/java/com/zoe/seconddemo/DialogActivity.java
@@ -0,0 +1,12 @@
+package com.zoe.seconddemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DialogActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_activity);
+    }
+}
